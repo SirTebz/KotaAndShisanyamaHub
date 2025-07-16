04acc564061f2e17f1a74acdc83c98278f192f1b
Teboho Mokgosi <62904711+SirTebz@users.noreply.github.com>
2025-07-16 15:27:48 +0200

    Implement Cart and Coupon Frontend integration

--- KotaAndShisanyamaHub.Services.ShoppingCartAPI/Program.cs
+++ KotaAndShisanyamaHub.Services.ShoppingCartAPI/Program.cs
@@ -3,6 +3,7 @@ using KotaAndShisanyamaHub.Services.ShoppingCartAPI;
 using KotaAndShisanyamaHub.Services.ShoppingCartAPI.Data;
 using KotaAndShisanyamaHub.Services.ShoppingCartAPI.Service;
 using KotaAndShisanyamaHub.Services.ShoppingCartAPI.Service.IService;
+using KotaAndShisanyamaHub.Services.ShoppingCartAPI.Utility;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -19,9 +20,13 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddScoped<ICouponService, CouponService>();
-builder.Services.AddHttpClient("Product", u => u.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
-builder.Services.AddHttpClient("Coupon", u => u.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
+builder.Services.AddHttpClient("Product", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:ProductAPI"])).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
+builder.Services.AddHttpClient("Coupon", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:CouponAPI"])).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
